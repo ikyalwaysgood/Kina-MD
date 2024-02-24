6f1d40f015371da308ac02c3b26ee2b23cf51a6e
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-25 01:07:13 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,8 +5,8 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `ryzen@ryzendesu.com`, `🇮🇩 Indonesia`, `📍 https://www.ryzendesu.com`, `👤 Owner Nao Bot`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `support@ryzendesu.com`, `🇮🇩 Indonesia`, `📍 https://github.com/ShirokamiRyzen/Nao-MD`, `Hanya bot biasa yang kadang error ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `!`, `🇮🇩 Indonesia`, `📍 !`, `👤 Owner Kιɳα Bσƚ`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `!`, `🇮🇩 Indonesia`, `📍 Instagram : iky_alwaysgood`, `Hanya bot biasa yang kadang error/mati 😁`]
   ], fkontak)
   await m.reply(`Hello @${m.sender.split(`@`)[0]} Thats my owner, dont spam or i will block u`)
   } 
